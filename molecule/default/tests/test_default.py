91831caafda368a10d1e93f9b2d7ac6b03cbd887
iavorskiy <33763105+iavorskiy@users.noreply.github.com>
2018-06-20 14:41:41 +0300

    Update test_default.py

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,7 +13,7 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
-def test_service_is_running(host):
-    assert host.service('nginx').is_running
 
-    
+#def test_service_is_running(host):
+#    assert host.service('nginx').is_running
+
